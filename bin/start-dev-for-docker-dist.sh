176ab50cb34f99b1f27704e953968858cd55010c
takuya-isbs <57710912+takuya-isbs@users.noreply.github.com>
2025-02-16 03:11:34 +0900

    start-dev-for-docker-dist.sh: update regexp

--- bin/start-dev-for-docker-dist.sh
+++ bin/start-dev-for-docker-dist.sh
@@ -9,12 +9,14 @@ LOAD_FROM_ENV=1
 if [ $LOAD_FROM_ENV -eq 1 ]; then
     source $CONF
     # get keys
-    for line in $(grep -v '^\s*#' $CONF); do
-        key=$(echo "$line" | cut -d '=' -f 1)
-        eval "export $key"
-    done
+    while IFS= read -r line; do
+        if echo "$line" | grep -qv '^\s*#\|^\s*$'; then
+            key=$(echo "$line" | cut -d '=' -f 1)
+            eval "export $key"
+        fi
+    done < "$CONF"
 else
-    GFARM_HTTP_CONFIG_FILE=$CONF
+    GFARM_HTTP_CONFIG_FILE="$CONF"
     export GFARM_HTTP_CONFIG_FILE
 fi
 
