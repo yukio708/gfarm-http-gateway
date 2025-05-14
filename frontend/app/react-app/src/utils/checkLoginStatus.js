abd4fc4549f749a69135b5a37349395d75710862
otsukay <otsukay@soum.co.jp>
2025-05-14 16:26:43 +0900

    change res.json() to res.text()

--- frontend/app/react-app/src/utils/checkLoginStatus.js
+++ frontend/app/react-app/src/utils/checkLoginStatus.js
@@ -4,9 +4,10 @@ async function checkLoginStatus() {
     try {
         const res = await fetch(`${API_URL}/c/me`, { credentials: "include" });
         if (!res.ok) throw new Error("not logged in");
-        const data = await res.json();
+        const data = await res.text();
         return data;
     } catch (err) {
+        console.error("Error fetching /c/me:", err);
         return null;
     }
 }
