c93d4360295081a7339fd9c4ff753a97c3192fa7
otsukay <otsukay@soum.co.jp>
2025-06-30 13:19:06 +0900

    update ConflictResolutionModal

--- frontend/app/react-app/src/components/ConflictResolutionModal.jsx
+++ frontend/app/react-app/src/components/ConflictResolutionModal.jsx
@@ -49,14 +49,21 @@ function ConflictResolutionModal({
     const handleCheck = (event, item, key) => {
         setIncomingItems((prev) =>
             prev.map((incomingItem) => {
-                if (incomingItem.name === item.name) {
+                if (incomingItem.name === item.name || incomingItem.dirPath === item.dirPath) {
                     return {
                         ...incomingItem,
                         [key]: event.target.checked,
                     };
                 }
-                if (incomingItem.dirPath === item.dirPath) {
-                    console.debug("incomingFile.dirPath === file.dirPath");
+                return incomingItem;
+            })
+        );
+    };
+
+    const handleSelectAll = (event, key) => {
+        setIncomingItems((prev) =>
+            prev.map((incomingItem) => {
+                if (incomingItem.parent_is_conflicted || incomingItem.is_conflicted) {
                     return {
                         ...incomingItem,
                         [key]: event.target.checked,
@@ -75,54 +82,121 @@ function ConflictResolutionModal({
             title={<h5 className="modal-title">File Name Conflict</h5>}
             text={
                 <div>
-                    Choose which files to keep. <br />
-                    Selecting both will rename the incoming file. <br />
-                    <ul className="list-group mt-3 shadow-sm">
+                    <div className="mb-2">
+                        Choose which files to keep. <br />
+                        Selecting both will rename the incoming file.
+                    </div>
+
+                    {/* Select all checkboxes */}
+                    <div className="mb-3">
+                        <div className="row">
+                            <div className="col">
+                                <input
+                                    type="checkbox"
+                                    className="form-check-input me-2"
+                                    id="selectAllCurrent"
+                                    onChange={(e) => handleSelectAll(e, "keep_current")}
+                                    checked={
+                                        showItems.filter(
+                                            (item) =>
+                                                item.is_conflicted || item.parent_is_conflicted
+                                        ).length ===
+                                        showItems.filter((item) => item.keep_current).length
+                                    }
+                                />
+                                <label className="form-check-label" htmlFor="selectAllCurrent">
+                                    Select all current
+                                </label>
+                            </div>
+                            <div className="col">
+                                <input
+                                    type="checkbox"
+                                    className="form-check-input me-2"
+                                    id="selectAllIncoming"
+                                    onChange={(e) => handleSelectAll(e, "keep_incoming")}
+                                    checked={
+                                        showItems.filter(
+                                            (item) =>
+                                                item.is_conflicted || item.parent_is_conflicted
+                                        ).length ===
+                                        showItems.filter((item) => item.keep_incoming).length
+                                    }
+                                />
+                                <label className="form-check-label" htmlFor="selectAllIncoming">
+                                    Select all incoming
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <ul className="list-group mt-2 shadow-sm">
                         {showItems.map(
                             (item, i) =>
                                 item.is_conflicted && (
                                     <li key={i} className="list-group-item list-group-item-action">
-                                        <div className="d-flex justify-content-between align-items-center">
+                                        <div className="d-flex justify-content-between align-items-center mb-2">
                                             <strong>{item.name}</strong>
                                             <span className="badge bg-warning text-dark">
                                                 Conflict
                                             </span>
                                         </div>
-                                        <div className="form-check mt-2">
-                                            <input
-                                                type="checkbox"
-                                                className="form-check-input me-2"
-                                                id={`current-${item.name}`}
-                                                onChange={(e) =>
-                                                    handleCheck(e, item, "keep_current")
-                                                }
-                                            />
-                                            <label
-                                                className="form-check-label"
-                                                htmlFor={`current-${item.name}`}
-                                            >
-                                                Keep current -{" "}
-                                                {formatFileSize(item.size, item.is_dir)},{" "}
-                                                {item.mtime_str}
-                                            </label>
-                                        </div>
-                                        <div className="form-check mt-2">
-                                            <input
-                                                type="checkbox"
-                                                className="form-check-input me-2"
-                                                id={`incoming-${item.name}`}
-                                                onChange={(e) =>
-                                                    handleCheck(e, item, "keep_incoming")
-                                                }
-                                            />
-                                            <label
-                                                className="form-check-label"
-                                                htmlFor={`incoming-${item.name}`}
-                                            >
-                                                Keep incoming -{" "}
-                                                {formatFileSize(item.current_size, item.is_dir)},{" "}
-                                                {item.current_mtime_str}
-                                            </label>
+
+                                        <div className="row">
+                                            <div className="col">
+                                                <div className="form-check">
+                                                    <input
+                                                        type="checkbox"
+                                                        className="form-check-input me-2"
+                                                        id={`current-${item.name}`}
+                                                        onChange={(e) =>
+                                                            handleCheck(e, item, "keep_current")
+                                                        }
+                                                        checked={item.keep_current}
+                                                    />
+                                                    <label
+                                                        className="form-check-label"
+                                                        htmlFor={`current-${item.name}`}
+                                                    >
+                                                        Keep current
+                                                    </label>
+                                                    <div className="small">
+                                                        Size:{" "}
+                                                        {formatFileSize(item.size, item.is_dir)}
+                                                    </div>
+                                                    <div className="small">
+                                                        Modified: {item.mtime_str}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="col">
+                                                <div className="form-check">
+                                                    <input
+                                                        type="checkbox"
+                                                        className="form-check-input me-2"
+                                                        id={`incoming-${item.name}`}
+                                                        onChange={(e) =>
+                                                            handleCheck(e, item, "keep_incoming")
+                                                        }
+                                                        checked={item.keep_incoming}
+                                                    />
+                                                    <label
+                                                        className="form-check-label"
+                                                        htmlFor={`incoming-${item.name}`}
+                                                    >
+                                                        Keep incoming
+                                                    </label>
+                                                    <div className="small">
+                                                        Size:{" "}
+                                                        {formatFileSize(
+                                                            item.current_size,
+                                                            item.is_dir
+                                                        )}
+                                                    </div>
+                                                    <div className="small">
+                                                        Modified: {item.current_mtime_str}
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </li>
                                 )
