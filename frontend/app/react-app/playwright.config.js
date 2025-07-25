4d5aad024fcdc79f7ac78810cd34d11abeab1894
otsukay <otsukay@soum.co.jp>
2025-07-25 16:08:50 +0900

    update playwright.config.js

--- frontend/app/react-app/playwright.config.js
+++ frontend/app/react-app/playwright.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: "html",
+    reporter: [["html", { outputFolder: "playwright-report", open: "on-failure" }]],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
@@ -31,11 +31,6 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
-    },
-
-    webServer: {
-        command: "npm run start",
-        port: 3000,
     },
 
     /* Configure projects for major browsers */
@@ -77,9 +72,9 @@ export default defineConfig({
     ],
 
     /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://localhost:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
+    webServer: {
+        command: "npm run start",
+        port: 3000,
+        reuseExistingServer: !process.env.CI,
+    },
 });
