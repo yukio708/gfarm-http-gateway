ad68ebc5c325216254519672de67fd35cd80d892
otsukay <otsukay@soum.co.jp>
2025-08-21 12:01:47 +0900

    update gfarm-http-gateway-test.sh and gfarm-http-gateway-dev-for-docker-dist.sh

--- server/bin/gfarm-http-gateway-dev-for-docker-dist.sh
+++ server/bin/gfarm-http-gateway-dev-for-docker-dist.sh
@@ -2,25 +2,33 @@
 set -eu
 set -x
 
-cp gfarm-http-gateway.conf.default gfarm-http-gateway.conf
+# cp gfarm-http-gateway.conf.default gfarm-http-gateway.conf
 
 DIR=$(dirname $(realpath $0))
-CONF=./gfarm-http-gateway-for-docker-dist.conf
+# CONF=./gfarm-http-gateway-for-docker-dist.conf
 
 LOAD_FROM_ENV=1
 
-if [ $LOAD_FROM_ENV -eq 1 ]; then
-    source $CONF
-    # get keys
-    while IFS= read -r line; do
-        if echo "$line" | grep -qv '^\s*#\|^\s*$'; then
-            key=$(echo "$line" | cut -d '=' -f 1)
-            eval "export $key"
-        fi
-    done < "$CONF"
-else
-    GFARM_HTTP_CONFIG_FILE="$CONF"
-    export GFARM_HTTP_CONFIG_FILE
-fi
+Load_Env () {
+    local CONF="$1"
+    [ -f "$CONF" ] || { echo "Config not found: $CONF" >&2; return 1; }
+
+    if [ $LOAD_FROM_ENV -eq 1 ]; then
+        source $CONF
+        # get keys
+        while IFS= read -r line; do
+            if echo "$line" | grep -qv '^\s*#\|^\s*$'; then
+                key=$(echo "$line" | cut -d '=' -f 1)
+                eval "export $key"
+            fi
+        done < "$CONF"
+    else
+        GFARM_HTTP_CONFIG_FILE="$CONF"
+        export GFARM_HTTP_CONFIG_FILE
+    fi
+}
+
+Load_Env ./gfarm-http-gateway.conf.default 
+Load_Env ./gfarm-http-gateway-for-docker-dist.conf
 
 exec bash -x ${DIR}/gfarm-http-gateway-dev.sh "$@"
--- server/bin/gfarm-http-gateway-test.sh
+++ server/bin/gfarm-http-gateway-test.sh
@@ -3,6 +3,10 @@
 DIR=$(realpath $(dirname $0))
 source "${DIR}/gfarm-http-gateway-common.sh"
 
+LOAD_FROM_ENV=1
+
+export GFARM_HTTP_CONFIG_FILE=./gfarm-http-gateway.conf.default 
+
 export GFARM_HTTP_SESSION_SECRET="qU70WDyIpXdSOT9/7l0hICy0597EPRs/aPb5Mj5Xniw="
 export GFARM_HTTP_OIDC_CLIENT_ID=TEST_CLIENT
 export GFARM_HTTP_OIDC_BASE_URL=http://keycloak.test/
