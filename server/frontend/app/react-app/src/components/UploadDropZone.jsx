0db95e3087556681f37ed20db28ed963f5437786
otsukay <otsukay@soum.co.jp>
2025-10-01 15:39:22 +0900

    server: ui: add data-testid

--- server/frontend/app/react-app/src/components/UploadDropZone.jsx
+++ server/frontend/app/react-app/src/components/UploadDropZone.jsx
@@ -164,7 +164,7 @@ function UploadDropZone({ onUpload, uploadDir, currentItems }) {
         if (!selectedItems.length) return null;
 
         return (
-            <div className="modal-body p-0">
+            <div className="modal-body p-0" data-testid="dropzone-modal">
                 <ul
                     className="list-unstyled m-0 p-3"
                     style={{ maxHeight: "40vh", overflowY: "auto", overscrollBehavior: "contain" }}
