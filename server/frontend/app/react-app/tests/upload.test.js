3713c17c23dc1e2886994413056f5f0941bb5a58
otsukay <otsukay@soum.co.jp>
2025-10-02 18:45:12 +0900

    server: ui: update test

--- server/frontend/app/react-app/tests/upload.test.js
+++ server/frontend/app/react-app/tests/upload.test.js
@@ -30,7 +30,7 @@ async function waitForProgressView(page, expectedFileName) {
 
     await expect(taskCard.locator("h6")).toContainText(expectedFileName);
 
-    const progressBar = taskCard.locator(".progress-bar");
+    const progressBar = progressView.locator(".progress-bar");
     await expect(progressBar).toBeVisible();
     await expect(taskCard.locator(".badge")).toHaveText("completed");
 }
